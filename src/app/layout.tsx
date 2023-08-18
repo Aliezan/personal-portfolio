6a7b4267d0caa746e759c088016cd36208756153
Alieza Nuriman <muhammadalieza4@gmail.com>
2023-08-18 09:22:16 +0700

    feat: Added shadcn-ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Navbar from '@/components/Navbar';
-import '@/app/globals.css';
+import '@/styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
