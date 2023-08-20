489a54c92ab7d2cf548069814f59cb6ca768e4d5
Alieza Nuriman <muhammadalieza4@gmail.com>
2023-08-20 13:36:24 +0700

    fix: shadcn-ui issues

---
+++ src/components/ui/NavigationMenuList.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import * as NavigationMenuPrimitive from '@radix-ui/react-navigation-menu';
+import { cn } from '@/lib/utils';
+
+export const NavigationMenuList = React.forwardRef<
+  HTMLUListElement,
+  React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Root> & {
+    className: string;
+  }
+>(({ className, ...props }, ref) => (
+  <NavigationMenuPrimitive.List
+    ref={ref}
+    className={cn(
+      'group flex flex-1 list-none items-center justify-center space-x-1',
+      className,
+    )}
+    {...props}
+  />
+));
