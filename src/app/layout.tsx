9a0936feeb2906d4f741abc9e4f0e7f9719aebea
Alieza Nuriman <115769623+Aliezan@users.noreply.github.com>
2024-03-17 03:12:10 +0700

    fix: complying metadata with opengraph standards

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
 import { FC } from "react";
-import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import ThemeProvider from "@/utils/theme-provider";
 import UIProvider from "@/utils/UIProvider";
 import "@/styles/globals.css";
 import Navibar from "@/components/navbar/Navibar";
-
-export const metadata: Metadata = {
-  title: {
-    default: "Muhammad Alieza Nuriman - Portfolio",
-    template: "%s | Alieza Nuriman - Portfolio",
-  },
-  description:
-    "Personal Portfolio of Muhammad Alieza Nuriman, Web Developer and Undergraduate Student at Politeknik Negeri Jakarta. Currently learning JavaScript modern web frameworks",
-  metadataBase: new URL("https://aliezan.is-a.dev/"),
-};
 
 interface RootLayoutProps {
   children: React.ReactNode;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,23 @@
 import React, { FC } from "react";
-import { Metadata } from "next";
+import HeadMetaData from "@/components/HeadMetaData";
 import About from "./_sections/About";
 import Hero from "./_sections/Hero";
 import Footer from "./_sections/Footer";
 import Works from "./_sections/Works";
 import ContactMe from "./_sections/ContactMe";
 
-export const metadata: Metadata = {
-  title: "Home",
-};
-
 const Home: FC = () => (
-  <main>
+  <>
+    <HeadMetaData
+      title="Muhammad Alieza Nuriman - Portfolio"
+      metaDescription="Personal Portfolio of Muhammad Alieza Nuriman, Web Developer and Undergraduate Student at Politeknik Negeri Jakarta. Currently learning JavaScript modern web frameworks"
+    />
     <Hero />
     <About />
     <Works />
     <ContactMe />
     <Footer />
-  </main>
+  </>
 );
 
 export default Home;
