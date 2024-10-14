9c20f3c7c6140edb8968d6c2fe68e2f12ea651cb
Alieza Nuriman <115769623+Aliezan@users.noreply.github.com>
2024-10-15 00:31:26 +0700

    fix: lifted the fetching logic

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,15 +1,32 @@
 import React, { FC } from "react";
 import BlogSection from "@/components/blogs/BlogSection";
 import BlogHero from "@/components/blogs/BlogHero";
+import { getClient } from "@/lib/apollo-server";
+import { getBlogPosts } from "@/query/schema";
 
-export const revalidate = process.env.NODE_ENV === "development" ? 0 : 5;
+export const revalidate = process.env.NODE_ENV === "development" ? 5 : 5;
 
 const Blogs: FC<{
   searchParams?: { [key: string]: string | undefined };
-}> = ({ searchParams }) => (
-  <main>
-    <BlogHero />
-    <BlogSection page={searchParams?.page ?? "1"} />
-  </main>
-);
+}> = async ({ searchParams }) => {
+  const page = searchParams?.page ?? "1";
+
+  const { data, error } = await getClient().query({
+    query: getBlogPosts,
+    variables: {
+      pagination: {
+        page: +page,
+        pageSize: 5,
+      },
+    },
+    fetchPolicy: "network-only",
+  });
+
+  return (
+    <main>
+      <BlogHero />
+      <BlogSection page={page} data={data} error={error} />
+    </main>
+  );
+};
 export default Blogs;
--- src/components/blogs/BlogSection.tsx
+++ src/components/blogs/BlogSection.tsx
@@ -2,86 +2,75 @@
 import React, { FC } from "react";
 import BlogCard from "@/components/blogs/BlogCard";
 import BlogPagination from "@/components/blogs/BlogPagination";
-import { getClient } from "@/lib/apollo-server";
-import { getBlogPosts } from "@/query/schema";
-import { env } from "@/env/server";
 import { SpaceGrotesk } from "@/utils/font";
 import { CircleAlert, TriangleAlert } from "lucide-react";
+import { BlogsQueryQuery } from "@/gql/graphql";
+import type { ApolloError } from "@apollo/client";
+import { env } from "@/env/server";
 
 const STRAPI_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:1337"
     : env.NEXT_PUBLIC_STRAPI_URL;
 
-const BlogSection: FC<{ page: string }> = async ({ page }) => {
-  const { data, error } = await getClient().query({
-    query: getBlogPosts,
-    variables: {
-      pagination: {
-        page: +page,
-        pageSize: 5,
-      },
-    },
-    fetchPolicy: "network-only",
-  });
-
-  return (
-    <section className="mt-5 px-7">
-      <div className="grid justify-center">
-        <div className="grid h-[996px] grid-rows-5 gap-6">
-          {error ? (
-            <div className="mt-40">
-              <div className="flex gap-4">
-                <TriangleAlert size={50} />
-                <h1
-                  className={`${SpaceGrotesk.className} text-center text-5xl`}
-                >
-                  Uh oh. Something went wrong!
-                </h1>
-              </div>
-              <p className="text-center">{error.message}</p>
-            </div>
-          ) : !data || !data.blogs || data.blogs.length === 0 ? (
-            <div className="mt-40">
-              <div className="flex gap-4">
-                <CircleAlert size={50} />
-                <h1
-                  className={`${SpaceGrotesk.className} text-center text-5xl`}
-                >
-                  No blog posts available
-                </h1>
-              </div>
-              <p className="mt-3 text-center">
-                Check back later for new content!
-              </p>
-            </div>
-          ) : (
-            data.blogs.map((blog) => {
-              const paragraph = blog?.content.filter(
-                (item: { type: string }) => item.type === "paragraph",
-              );
-
-              return (
-                <BlogCard
-                  key={blog?.documentId}
-                  date={blog?.date ?? "NO DATE"}
-                  imgUrl={STRAPI_URL.concat(blog?.image?.[0]?.url ?? "")}
-                  alt={blog?.image?.[0]?.url ?? "NO ALT PROVIDED"}
-                  title={blog?.title ?? "UNTITLED"}
-                  slug={blog?.slug ?? "NO SLUG"}
-                  content={paragraph[0].children[0].text}
-                />
-              );
-            })
-          )}
-        </div>
-      </div>
-      <BlogPagination
-        totalPages={data?.blogs_connection?.pageInfo.total!}
-        page={+page}
-      />
-    </section>
-  );
+type BlogSectionProps = {
+  page: string;
+  data: BlogsQueryQuery;
+  error: ApolloError | undefined;
 };
+
+const BlogSection: FC<BlogSectionProps> = ({ page, data, error }) => (
+  <section className="mt-5 px-7">
+    <div className="grid justify-center">
+      <div className="grid h-[996px] grid-rows-5 gap-6">
+        {error ? (
+          <div className="mt-40">
+            <div className="flex gap-4">
+              <TriangleAlert size={50} />
+              <h1 className={`${SpaceGrotesk.className} text-center text-5xl`}>
+                Uh oh. Something went wrong!
+              </h1>
+            </div>
+            <p className="text-center">{error.message}</p>
+          </div>
+        ) : !data || !data.blogs || data.blogs.length === 0 ? (
+          <div className="mt-40">
+            <div className="flex gap-4">
+              <CircleAlert size={50} />
+              <h1 className={`${SpaceGrotesk.className} text-center text-5xl`}>
+                No blog posts available
+              </h1>
+            </div>
+            <p className="mt-3 text-center">
+              Check back later for new content!
+            </p>
+          </div>
+        ) : (
+          data.blogs.map((blog) => {
+            const paragraph = blog?.content.filter(
+              (item: { type: string }) => item.type === "paragraph",
+            );
+
+            return (
+              <BlogCard
+                key={blog?.documentId}
+                date={blog?.date ?? "NO DATE"}
+                imgUrl={STRAPI_URL.concat(blog?.image?.[0]?.url ?? "")}
+                alt={blog?.image?.[0]?.url ?? "NO ALT PROVIDED"}
+                title={blog?.title ?? "UNTITLED"}
+                slug={blog?.slug ?? "NO SLUG"}
+                content={paragraph[0].children[0].text}
+              />
+            );
+          })
+        )}
+      </div>
+    </div>
+    <BlogPagination
+      totalPages={data?.blogs_connection?.pageInfo.total!}
+      page={+page}
+    />
+  </section>
+);
 
 export default BlogSection;
