38a8f0f7d3f38776ccd90769035a7b3746a58966
Alieza Nuriman <115769623+Aliezan@users.noreply.github.com>
2024-10-20 15:48:24 +0700

    fix: sitemap error

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,19 +1,19 @@
 import { MetadataRoute } from "next";
 import { env } from "@/env/server";
-import { getClient } from "@/lib/apollo-server";
-import { getBlogPosts } from "@/query/schema";
+// import { getClient } from "@/lib/apollo-server";
+// import { getBlogPosts } from "@/query/schema";
 
-const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
-  const { data } = await getClient().query({
-    query: getBlogPosts,
-  });
+const sitemap = async (): Promise<MetadataRoute.Sitemap> =>
+  // const { data } = await getClient().query({
+  //   query: getBlogPosts,
+  // });
 
-  const BlogPosts = data.blogs.map((blog) => ({
-    url: `${env.NEXT_PUBLIC_BASE_URL}/blogs/${blog?.documentId}`,
-    lastModified: new Date(blog?.updatedAt),
-  }));
+  // const BlogPosts = data.blogs.map((blog) => ({
+  //   url: `${env.NEXT_PUBLIC_BASE_URL}/blogs/${blog?.documentId}`,
+  //   lastModified: new Date(blog?.updatedAt),
+  // }));
 
-  return [
+  [
     {
       url: `${env.NEXT_PUBLIC_BASE_URL}/`,
       lastModified: new Date(),
@@ -22,8 +22,6 @@ const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
       url: `${env.NEXT_PUBLIC_BASE_URL}/blogs`,
       lastModified: new Date(),
     },
-    ...BlogPosts,
+    // ...BlogPosts,
   ];
-};
-
 export default sitemap;
