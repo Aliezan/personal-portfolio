6cdaedcca529a8195873159eb22db0e135c4ece4
Alieza Nuriman <115769623+Aliezan@users.noreply.github.com>
2024-12-10 21:09:14 +0700

    fix: copy button bg

--- src/components/blogs/Pre.tsx
+++ src/components/blogs/Pre.tsx
@@ -7,7 +7,7 @@ import clsx from "clsx";
 const Pre: FC = ({
   children,
   raw,
-  buttonClasses = "absolute top-3 right-3 bg-zinc-900",
+  buttonClasses = "absolute top-3 right-3",
   ...props
 }) => {
   return (
