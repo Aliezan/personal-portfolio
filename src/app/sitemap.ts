eb5a4993bf703454b2c6f4614fbb6820395f4448
Alieza Nuriman <115769623+Aliezan@users.noreply.github.com>
2024-12-14 08:50:51 +0700

    chore: temp fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,46 +1,46 @@
 import { MetadataRoute } from "next";
 import { env } from "@/env/server";
-import { getClient } from "@/lib/apollo-server";
-import { getAllBlogPosts } from "@/query/schema";
+// import { getClient } from "@/lib/apollo-server";
+// import { getAllBlogPosts } from "@/query/schema";
+
+// const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
+//   const { data, error } = await getClient().query({
+//     query: getAllBlogPosts,
+//   });
+
+//   if (!data || error) {
+//     return [
+//       {
+//         url: `${env.NEXT_PUBLIC_BASE_URL}/`,
+//         lastModified: new Date(),
+//       },
+//     ];
+//   }
+
+//   const BlogPosts = data.blogs.map((blog) => ({
+//     url: `${env.NEXT_PUBLIC_BASE_URL}/blogs/${blog?.documentId}`,
+//     lastModified: new Date(blog?.updatedAt),
+//   }));
+
+//   return [
+//     {
+//       url: `${env.NEXT_PUBLIC_BASE_URL}/`,
+//       lastModified: new Date(),
+//     },
+//     {
+//       url: `${env.NEXT_PUBLIC_BASE_URL}/blogs`,
+//       lastModified: new Date(),
+//     },
+//     ...BlogPosts,
+//   ];
+// };
 
 const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
-  const { data, error } = await getClient().query({
-    query: getAllBlogPosts,
-  });
-
-  if (!data || error) {
-    return [
-      {
-        url: `${env.NEXT_PUBLIC_BASE_URL}/`,
-        lastModified: new Date(),
-      },
-    ];
-  }
-
-  const BlogPosts = data.blogs.map((blog) => ({
-    url: `${env.NEXT_PUBLIC_BASE_URL}/blogs/${blog?.documentId}`,
-    lastModified: new Date(blog?.updatedAt),
-  }));
-
-  if (error || !data) {
-    return [
-      {
-        url: `${env.NEXT_PUBLIC_BASE_URL}/`,
-        lastModified: new Date(),
-      },
-    ];
-  }
-
   return [
     {
       url: `${env.NEXT_PUBLIC_BASE_URL}/`,
       lastModified: new Date(),
     },
-    {
-      url: `${env.NEXT_PUBLIC_BASE_URL}/blogs`,
-      lastModified: new Date(),
-    },
-    ...BlogPosts,
   ];
 };
 export default sitemap;
