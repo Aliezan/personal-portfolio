df955b6fefe32cbad81a384cdb3af73d95d434ff
Alieza Nuriman <115769623+Aliezan@users.noreply.github.com>
2025-02-19 18:45:30 +0700

    fix: add description

--- src/data/blogList.ts
+++ src/data/blogList.ts
@@ -4,7 +4,8 @@ const blogData = [
     title: "Easy VPS Setup and Deployments with Coolify",
     createdAt: "Saturday, December 14, 2024",
     lastModified: "Saturday, December 14, 2024",
-    description: "",
+    description:
+      "Tired of the hassle of provisioning servers? Frustrated with manually configuring daemon process managers and proxy servers only to end up with a messy setup? Worry no more! Coolify simplifies infrastructure management and maintenance, making deployment a breeze!",
     slug: "easy-vps-setup-coolify",
     thumbnailURL: "/blogpost1_thumbnail.webp",
     alt: "VPS Setup and Deployments",
